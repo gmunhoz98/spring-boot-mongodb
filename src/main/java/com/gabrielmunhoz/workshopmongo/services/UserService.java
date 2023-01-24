3a7cb8173ee063a5ed57e0faac5633e2f7b6401b
Gabriel Munhoz <GabrielMunhoz@UNION.icarotech.com>
2023-01-24 18:26:11 -0300

    Obtendo usuario FindById

--- src/main/java/com/gabrielmunhoz/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrielmunhoz/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.gabrielmunhoz.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabrielmunhoz.workshopmongo.domain.User;
 import com.gabrielmunhoz.workshopmongo.repository.UserRepository;
+import com.gabrielmunhoz.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
