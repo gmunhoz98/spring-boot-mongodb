d6e183c1352f19929ed074659c52acca4bc13de2
Gabriel Munhoz <GabrielMunhoz@UNION.icarotech.com>
2023-01-24 17:53:42 -0300

    Padrao DTO para retornar Usuarios

--- src/main/java/com/gabrielmunhoz/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabrielmunhoz/workshopmongo/resources/UserResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gabrielmunhoz.workshopmongo.DTO.UserDTO;
 import com.gabrielmunhoz.workshopmongo.domain.User;
 import com.gabrielmunhoz.workshopmongo.services.UserService;
 
@@ -19,8 +20,9 @@ public class UserResource {
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).toList();
+		return ResponseEntity.ok().body(listDto);
 	}
 }
