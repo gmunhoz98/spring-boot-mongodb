b8bd579d5c3eb891c0ec1f57cb6c4903c8bbf231
Gabriel Munhoz <GabrielMunhoz@UNION.icarotech.com>
2023-01-25 16:31:01 -0300

    Obtendo post FindById

---
+++ src/main/java/com/gabrielmunhoz/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.gabrielmunhoz.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielmunhoz.workshopmongo.domain.Post;
+import com.gabrielmunhoz.workshopmongo.repository.PostRepository;
+import com.gabrielmunhoz.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> user = repo.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+    }	
+
+}
