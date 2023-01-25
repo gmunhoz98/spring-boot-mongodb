ab884dd17b019fb82d1ede304be01623f26308a5
Gabriel Munhoz <GabrielMunhoz@UNION.icarotech.com>
2023-01-25 16:25:00 -0300

    Endpoint Posts do Usuario

--- src/main/java/com/gabrielmunhoz/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabrielmunhoz/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gabrielmunhoz.workshopmongo.DTO.UserDTO;
+import com.gabrielmunhoz.workshopmongo.domain.Post;
 import com.gabrielmunhoz.workshopmongo.domain.User;
 import com.gabrielmunhoz.workshopmongo.services.UserService;
 
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
