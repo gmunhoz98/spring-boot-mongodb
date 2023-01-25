768af0e7c54604e29a7c972f5a4057fa434b56b6
Gabriel Munhoz <GabrielMunhoz@UNION.icarotech.com>
2023-01-25 19:29:04 -0300

    Consulta com varios criterios

--- src/main/java/com/gabrielmunhoz/workshopmongo/resources/PostResource.java
+++ src/main/java/com/gabrielmunhoz/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.gabrielmunhoz.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,17 @@ public class PostResource {
 		text = URL.decodeParam(text);
 
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0l));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 }
