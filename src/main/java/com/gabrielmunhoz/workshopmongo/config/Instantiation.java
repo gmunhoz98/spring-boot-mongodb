db1ffbb787f92b1be600563532f96319c0d9c105
Gabriel Munhoz <GabrielMunhoz@UNION.icarotech.com>
2023-01-25 16:15:14 -0300

    Referenciando os Posts do usuario

--- src/main/java/com/gabrielmunhoz/workshopmongo/config/Instantiation.java
+++ src/main/java/com/gabrielmunhoz/workshopmongo/config/Instantiation.java
@@ -43,6 +43,9 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(user1));
 
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
+
+		user1.getPosts().addAll(Arrays.asList(post1, post2));
+		userReposiroty.save(user1);
 	}
 
 }
