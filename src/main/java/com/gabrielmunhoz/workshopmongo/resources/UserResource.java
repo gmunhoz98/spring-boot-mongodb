b688cc5180891c17f416532a40c5c53c374c73f7
Gabriel Munhoz <GabrielMunhoz@UNION.icarotech.com>
2023-01-25 10:41:24 -0300

    Deletando usuario com deleteById

--- src/main/java/com/gabrielmunhoz/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabrielmunhoz/workshopmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +37,12 @@ public class UserResource {
 		User obj = service.findById(id);
 
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 	@PostMapping
